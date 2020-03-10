e4686c073d4a01318d2c3bc9126332c4de409001
Your Name <magnuskonrad@gmail.com>
2020-03-10 21:43:42 +0000

    First unittest ready

---
+++ sample.py/sample.py
@@ -0,0 +1,10 @@
+class FizzBuzz:
+    def
+
+def fizzbuzz(number):
+    retStr = ""
+    if int(number) % 3 == 0:
+        retStr += "Fizz"
+    if int(number) % 5 == 0:
+        retStr += "Buzz"
+    elif 
---
+++ sample.py/test_sample.py
@@ -0,0 +1,14 @@
+import unittest
+from sample.py import fizzbuzz
+
+class MyFirstTests(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(fizzbuzz(5), 'Buzz')
+        self.assertEqual(fizzbuzz(6), 'Fizz')
+        self.assertEqual(fizzbuzz(15), 'FizzBuzz')
+        self.assertEqual(fizzbuzz(8), '8')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
